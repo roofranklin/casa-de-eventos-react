6dd2e7ef3ddabaf12ac2e408146fad9911eb7607
vitoralbq <vitor.albuquerque.mun@gmail.com>
2024-02-18 23:33:43 +0100

    css feito

--- src/pages/Login/login.style.jsx
+++ src/pages/Login/login.style.jsx
@@ -1,41 +1,96 @@
 import styled from 'styled-components';
-export const Container = styled.div`
-width: 1140px;
-height: calc(100vh - 240px);
-display: block;
-margin: auto;
-`;
+import { Link } from 'react-router-dom';
 
-export const Title = styled.h1`
-  text-align: center;
-  color: #000;
-`;
+export const LoginContainer = styled.div`
+    display: flex;
+    font-size: 15px;
+    line-height: 200%;
+    font-weight: 400;
+`
 
-export const Input = styled.input`
-  width: 100%;
-  margin-bottom: 10px;
-  padding: 8px;
-  box-sizing: border-box;
-`;
+export const LoginTitle = styled.h1`
+    font-family: 'Playfair Display', serif;
+    text-align: center;
+    font-size: 28px;
+    font-weight: 400;
+    color: #1F0049;
+`
 
-export const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: #4caf50;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  text-align: center;
+export const Sidebar = styled.aside`
+    color: #FFFFFF;
+    background-color: #1F0049;
+    height: 100vh;
+    width: 38%;
+`
+export const SidebarContainer = styled.div`
+    width: 500px;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`
 
-  &:hover {
-    background-color: #45a049;
-  }
-`;
+export const LogoContainer = styled.div``
 
-export const erro = styled.p`
-  color: red;
-  margin-top: 10px;
-  text-align: center;
-`;
+export const Logo = styled.img`
+    width: 100%;
+`
 
+export const LoginContent = styled.main`
+    width: 62%;
+    height: 100vh;
+    background-color: #E2D8A5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const LoginBox = styled.div`
+    width: 460px;
+`
+
+export const FormBox = styled.form``
+
+export const FormItem = styled.div`
+    padding-top: 20px;
+    &:last-of-type {
+        margin-bottom: 20px;
+    }
+`
+
+export const FormLabel = styled.label`
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 125%;
+`
+
+export const FormInput = styled.input`
+    display: block;
+    width: calc(100% - 30px);
+    margin-top: 5px;
+    padding: 0 15px;
+    border: none;
+    height: 36px;
+    font-size: 14px;
+    line-height: 150%;
+    font-weight: 400;
+    border-radius: 4px;
+    background-color: #f4f4f4;
+`
+
+export const FormButton = styled(Link)`
+    display: block;
+    margin: auto;
+    text-align: center;
+    width: 140px;
+    height: 44px;
+    border: 1px solid transparent;
+    border-radius: 22px;
+    background-color: #1F0049;
+    color: #FFFFFF;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 44px;
+    text-decoration: none;
+`
+
+export const ErrorMessage = styled.p``
